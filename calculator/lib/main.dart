33ecc372434aefe3abdd15315e39077a0ef61a69
Sreedhanya S <131782861+Sree-dhanya@users.noreply.github.com>
2023-05-20 22:19:47 +0530

    created calculator_

---
+++ calculator/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[100],
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              child: Center(
+                child: MyButton(
+                  color: Colors.deepPurple,
+                  textcolor: Colors.white,
+                  buttonText: '0',
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
